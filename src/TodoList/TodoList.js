8c72056a3f859e782ba6402bc0aca47467af896c
Clarik <clarik72@gmail.com>
2021-12-16 04:47:09 +0700

    ReactHook

---
+++ src/TodoList/TodoList.js
@@ -0,0 +1,14 @@
+import './TodoList.css'
+
+const Todolist = (props) => {
+    console.log(props.dataTodos);
+    return (
+        <ul>{
+            props.dataTodos.map((todo)=> {
+                return <li key={todo.id}>{todo.title}</li>
+            })
+        }</ul>
+    );
+}
+
+export default Todolist
